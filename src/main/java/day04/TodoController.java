838c92099f91a6ce223d2c29ff27cafb645068ae
hwanhee <hwanhee603562@gmail.com>
2023-10-05 09:55:48 +0900

    최초 커밋

---
+++ src/main/java/day04/TodoController.java
@@ -0,0 +1,32 @@
+package day04;
+
+import org.springframework.stereotype.Controller;
+
+// 스프링 빈 : 스프링 컨테이너(저장소)에 저장된 객체
+
+@Controller // Spring mvc 중 해당 클래스를 Controller로 사용
+public class TodoController {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
